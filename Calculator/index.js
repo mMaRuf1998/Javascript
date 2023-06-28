669d828a2df586e27a9292a7b8de04125b16d48a
mMarRuf1998 <41258916+mMarRuf1998@users.noreply.github.com>
2023-06-29 01:55:38 +0600

    Add files via upload
    A Basic Calculator with JS and Basic HTML CSS

---
+++ Calculator/index.js
@@ -0,0 +1,30 @@
+let num1 = 5
+let num2 = 2
+
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+
+
+function add()
+    {
+        document.getElementById("sum-el").textContent = "Sum : "+ (num1+num2) ;
+    }
+
+
+function subtract()
+    {
+        document.getElementById("sum-el").textContent= "Sum : "+ (num1-num2) ;
+    }
+
+
+function multiply()
+    {
+        document.getElementById("sum-el").textContent = "Sum : "+ (num1*num2) ;
+    }
+
+
+function divide()
+    {
+       
+        document.getElementById("sum-el").textContent = "Sum : "+ (num1/num2)  ;
+    }
